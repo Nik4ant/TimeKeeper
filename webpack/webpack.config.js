7d87e719dd3ef3cc0626e9757204954a03582e47
Nik4ant <Nik4ant@yandex.ru>
2022-05-22 21:01:29 +0700

    Webpack and TypeScript setup

---
+++ webpack/webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+module.exports = {
+    mode: "production",
+    entry: {
+        background: path.resolve(__dirname, "..", "src", "background.ts"),
+    },
+    output: {
+        path: path.join(__dirname, "../dist"),
+        filename: "[name].js",
+    },
+    resolve: {
+        extensions: [".ts", ".js"],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: "ts-loader",
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    plugins: [
+        new CopyWebpackPlugin({
+            patterns: [
+                {from: ".", to: ".", context: "public"},
+                {from: "src/Static", to: "./Static"}
+            ]
+        }),
+    ],
+};
